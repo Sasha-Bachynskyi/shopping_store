6e3e9d03f16fb67edc9b843e6ad50548bfff19d6
Sasha Bachynskyi <allfred12@gmail.com>
2024-02-17 12:37:42 +0200

    implemented routing

---
+++ src/app/recipes/recipe-added/recipe-added.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-recipe-added',
+  templateUrl: './recipe-added.component.html',
+  styleUrl: './recipe-added.component.scss'
+})
+export class RecipeAddedComponent {
+}
