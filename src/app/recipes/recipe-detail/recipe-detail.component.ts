68f406c23f5968a7365f500cbe8e2690dde8581f
Sasha Bachynskyi <allfred12@gmail.com>
2024-02-24 14:31:48 +0200

    implemented forms

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -40,4 +40,9 @@ export class RecipeDetailComponent implements OnInit, OnDestroy {
   onEditRecipe() {
     this.router.navigate(['edit'], {relativeTo: this.route});
   }
+
+  onDeleteRecipe() {
+    this.recipeService.deleteRecipe(this.id);
+    this.router.navigate(['/recipes']);
+  }
 }
